c2487280033a97f2c96527048544aeb79d4dfdd4
lhchavez <lhchavez@lhchavez.com>
2022-07-09 02:42:03 +0200

    Non-forwardable tokens: support for specifying the audience (#3)
    This change makes it possible to generate non-forwardable tokens. Tokens
    now all contain the intended recipient (the audience) so that they
    cannot be forwarded and spoof the identity of the sender.

--- identity_test.go
+++ identity_test.go
@@ -13,13 +13,50 @@ func Example() {
 		fmt.Println("Sorry, this repl does not yet have an identity (anonymous run?).")
 		return
 	}
+	identityKey := os.Getenv("REPL_IDENTITY_KEY")
+	if identity == "" {
+		fmt.Println("Sorry, this repl does not yet have an identity (anonymous run?).")
+		return
+	}
 
-	replIdentity, err := replidentity.VerifyIdentity(identity, replidentity.ReadPublicKeyFromEnv)
+	// This should be set to the Repl ID of the repl you want to prove your
+	// identity to.
+	targetRepl := "target_repl"
+
+	// Create a signing authority that is authorized to emit tokens for the
+	// current repl.
+	signingAuthority, err := replidentity.NewSigningAuthority(
+		string(identityKey),
+		identity,
+		os.Getenv("REPL_ID"),
+		replidentity.ReadPublicKeyFromEnv,
+	)
+	if err != nil {
+		panic(err)
+	}
+
+	signedToken, err := signingAuthority.Sign(targetRepl)
+	if err != nil {
+		panic(err)
+	}
+
+	// Verify the signed token, pretending we are the target repl.
+	replIdentity, err := replidentity.VerifyIdentity(
+		signedToken,
+		targetRepl,
+		replidentity.ReadPublicKeyFromEnv,
+	)
 	if err != nil {
 		panic(err)
 	}
 
 	fmt.Println()
-	fmt.Printf("The identity in the repl's REPL_IDENTITY token (%d bytes) is:\n", len(identity))
-	fmt.Printf("repl id: %s\n   user: %s\n   slug: %s\n", replIdentity.Replid, replIdentity.User, replIdentity.Slug)
+	fmt.Printf("The identity in the repl's token (%d bytes) is:\n", len(identity))
+	fmt.Printf(
+		"repl id: %s\n   user: %s\n   slug: %s  audience: %s\n",
+		replIdentity.Replid,
+		replIdentity.User,
+		replIdentity.Slug,
+		replIdentity.Aud,
+	)
 }
