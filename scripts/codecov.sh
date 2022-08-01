e9911bdd567a225a008a8e369d27d2696994df13
Matt Iselin <matthew@repl.it>
2022-08-01 15:56:07 -0700

    Add CI and code coverage reporting

---
+++ scripts/codecov.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+SHA="-C $SEMAPHORE_GIT_SHA"
+BRANCH="-B $SEMAPHORE_GIT_BRANCH"
+PR=
+if [ "x$SEMAPHORE_GIT_PR_NUMBER" != "x" ]; then
+    PR="-P $SEMAPHORE_GIT_PR_NUMBER"
+    SHA="-C $SEMAPHORE_GIT_PR_SHA"
+    BRANCH="-B $SEMAPHORE_GIT_PR_BRANCH"
+fi
+BUILD="-b $SEMAPHORE_JOB_ID"
+NAME="-n '$SEMAPHORE_JOB_NAME'"
+
+# Are we in a PR context? The variables are wrong if so.
+if [ "x$SEMAPHORE_GIT_REF_TYPE" == "xpull-request" ]; then
+    echo "codecov wrapper: this appears to be a PR named '$SEMAPHORE_GIT_PR_NAME', setting params accordingly..."
+fi
+
+./codecov $SHA $BRANCH $PR $BUILD -r $SEMAPHORE_GIT_REPO_SLUG -f 'coverage.out.*' "$@" || echo 'Failed to upload coverage data.'
+rm -f coverage.out.*
