d12d1b12568e4a7c0497bae6dbe6dcfe706fb00c
Matt Iselin <matthew@repl.it>
2022-08-01 16:19:47 -0700

    make sure we actually emit coverage data

--- scripts/codecov.sh
+++ scripts/codecov.sh
@@ -16,5 +16,5 @@ if [[ "$SEMAPHORE_GIT_REF_TYPE" == "pull-request" ]]; then
     echo "codecov wrapper: this appears to be a PR named '$SEMAPHORE_GIT_PR_NAME', setting params accordingly..."
 fi
 
-codecov $SHA $BRANCH $PR $BUILD -r $SEMAPHORE_GIT_REPO_SLUG -f 'coverage.out.*' "$@" || echo 'Failed to upload coverage data.'
-rm -f coverage.out.*
+codecov $SHA $BRANCH $PR $BUILD -r $SEMAPHORE_GIT_REPO_SLUG -f 'coverage.out' "$@" || echo 'Failed to upload coverage data.'
+rm -f coverage.out
