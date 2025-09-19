a30f1a9fe716ef72d4b6e7c826a7a55101c28048
lhchavez <lhchavez@lhchavez.com>
2025-09-19 04:32:44 +0000

    [paserk] Add functions to convert ed25519 keys to curve25519
    https://libsodium.gitbook.io/doc/advanced/ed25519-curve25519 has a nice
    algorithm (with a proof!) that says that it is possible to use an
    ed25119 key (used for signature) and make it a curve25519 (used for
    asymmetric encryption).
    
    This change adds two new functions that achieve this.

---
+++ keys.go
@@ -0,0 +1,30 @@
+package replidentity
+
+import (
+	"crypto/ed25519"
+	"crypto/sha512"
+
+	"filippo.io/edwards25519"
+	"golang.org/x/crypto/curve25519"
+)
+
+// ed25519PrivateKeyToCurve25519 converts a ed25519 private key in X25519 equivalent
+// source: https://github.com/FiloSottile/age/blob/980763a16e30ea5c285c271344d2202fcb18c33b/agessh/agessh.go#L287
+func Ed25519PrivateKeyToCurve25519(pk ed25519.PrivateKey) []byte {
+	h := sha512.New()
+	h.Write(pk.Seed())
+	out := h.Sum(nil)
+	return out[:curve25519.ScalarSize]
+}
+
+// ed25519PublicKeyToCurve25519 converts a ed25519 public key in X25519 equivalent
+// source: https://github.com/FiloSottile/age/blob/main/agessh/agessh.go#L190
+func Ed25519PublicKeyToCurve25519(pk ed25519.PublicKey) ([]byte, error) {
+	// See https://blog.filippo.io/using-ed25519-keys-for-encryption and
+	// https://pkg.go.dev/filippo.io/edwards25519#Point.BytesMontgomery.
+	p, err := new(edwards25519.Point).SetBytes(pk)
+	if err != nil {
+		return nil, err
+	}
+	return p.BytesMontgomery(), nil
+}
